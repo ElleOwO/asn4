0ace1cf22ca4205ea2e7c38c86b3762482e0aebc
Elle Mya <lmtdmya@gmail.com>
2021-02-28 05:49:54 +0000

    Add code and text file for homework

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#LYIN MYA
+#LYM284
+#11192838
+#COM145
+#SECTION04
+
 # CMPT 145 Course material
 # Copyright (c) 2017-2020 Michael C Horsch
 # All rights reserved.
@@ -28,7 +34,7 @@
 #       The calculations for var() and sampvar() were removed to 
 #       keep the exercise concise.
 
-class Statistics(object):
+class Statistics:
     def __init__(self):
         """
         Purpose:
@@ -36,7 +42,11 @@ class Statistics(object):
         """
         self.__count = 0      # how many data values seen so far
         self.__avg = 0        # the running average so far
-
+        self.__range = 0 #range seen so far
+        self.__mode = 0 #mode seen so far
+        self.__max = 0 #max value so far
+        self.__min = 0 #min value so far
+        self.__list_of_nums = [] #list of numbers so far
 
     def add(self, value):
         """
@@ -50,11 +60,20 @@ class Statistics(object):
         Return:
             :return none
         """
+        self.__list_of_nums.append(value)
         self.__count += 1
         k = self.__count           # convenience
         diff = value - self.__avg  # convenience
         self.__avg += diff / k
 
+        if self.__min is None:
+            self.__min = value
+            self.__max = value
+        else:
+            self.__min = min(value, self.__min)
+            self.__max = max(value, self.__max)
+
+        self.__range = (self.__max) - (self.__min)
 
     def mean(self):
         """
@@ -81,3 +100,75 @@ class Statistics(object):
                   This is clearly false.
         """
         return self.__count
+
+    def min(self):
+        """
+        Purpose:
+            Return the minimum of all the values seen so far.
+        Pre-conditions:
+            stat: the Statistics record
+        Post-conditions:
+            (none)
+        Return:
+            The minimum of the data seen so far.
+            Note: if no data has been seen, None is returned.
+        """
+        return self.__min
+
+    def max(self):
+        """
+        Purpose:
+            Return the maximum of all the values seen so far.
+        Pre-conditions:
+            stat: the Statistics record
+        Post-conditions:
+            (none)
+        Return:
+            The maximum of the data seen so far.
+            Note: if no data has been seen, None is returned.
+        """
+        return self.__max
+
+    def range(self):
+        """
+        Purpose:
+            Return the range of all the values seen so far.
+        Pre-conditions:
+            stat: the Statistics record
+        Post-conditions:
+            (none)
+        Return:
+            The range of the data seen so far.
+            Note: if no data has been seen, None is returned.
+        """
+        return self.__range
+
+    def mode(self):
+        """
+        Purpose:
+            Return the mode of all the values seen so far.
+        Pre-conditions:
+            stat: the Statistics record
+        Post-conditions:
+            (none)
+        Return:
+            The mode of the data seen so far.
+            Note: if no data has been seen, None is returned.
+        """
+        max_count = (0, 0)
+        for num in self.__list_of_nums:
+            occurences = self.__list_of_nums.count(num)
+            if occurences > max_count[0]:
+                max_count = (occurences, num)
+        return max_count[1]
+
+
+
+
+
+stat = Statistics()
+
+stat.add(1)
+stat.add(0)
+stat.add(2)
+print(stat.mode())
